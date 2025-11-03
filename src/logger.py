0d7dfd30eff4ffb7659573f396022aeee16738f8
mynteshav <teshavsharma74@gmail.com>
2025-11-04 01:52:45 +0530

    Logging and exception

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" # create a log file. 
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE) # get the log path
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE) #get the log file path.
+''' whenever we really want to create the log we really need to set see 
+if you override the functionality of the logging we have to probabily set this
+have into the basic config. '''
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format= "[%(asctime)s] %(lineno)d %(name)s- %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
